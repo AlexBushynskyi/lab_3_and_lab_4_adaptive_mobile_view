5d8cb59b0ac9dde6ac6a6105ee2982d991cac9bf
DangerWolf <36326717+AlexBushynskyi@users.noreply.github.com>
2018-11-12 22:37:18 +0200

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+var block_width;
+var block_height;
+var toogle_form = 1;
 
-toogle_form = 1;
+ $(window).resize(function() {
+ 	block_width = parseInt($('.all').css('width'));
+ 	if (toogle_form == 1) {
+ 		$('.img_main').css('height', "");
+ 	} else if (toogle_form == 0 && block_width <= 400) {
+ 		$('.img_main').css('height', '815px');
+ 	} else if (toogle_form == 0 && block_width > 400) {
+ 		$('.img_main').css('height', '685px');
+ 	}
+ });
+
 
 $('.join_us').click(function(){
+	block_width = parseInt($('.all').css('width'));
+	block_height = parseInt($('.img_main').css('height'));
 	if (toogle_form == 0){
 		$('.join_us').addClass('white_text').removeClass('black_text');
 		$('.sign_in').addClass('black_text').removeClass('white_text');
@@ -10,9 +25,14 @@ $('.join_us').click(function(){
 		$('.form_login').addClass('form_height_big').removeClass('form_height_small');
 		toogle_form = 1;
 	}
+	if (block_width < 800 && (block_height == 685 ||  block_height == 815)) {
+		$('.img_main').css('height', block_height+165);
+	}
 });
 
 $('.sign_in').click(function(){
+	block_width = parseInt($('.all').css('width'));
+	block_height = parseInt($('.img_main').css('height'));
 	if (toogle_form == 1){
 		$('.sign_in').addClass('white_text').removeClass('black_text');
 		$('.join_us').addClass('black_text').removeClass('white_text');
@@ -20,6 +40,9 @@ $('.sign_in').click(function(){
 		$('#join_us').addClass('visible_forms');
 		$('.form_login').addClass('form_height_small').removeClass('form_height_big');
 		toogle_form = 0;
+	}
+	if (block_width < 800 && (block_height == 850 ||  block_height == 980)) {
+		$('.img_main').css('height', block_height-165);
 	}
 });
 
